e4c931b8d178450806d7d72bbb0f04b351d32d05
Mr. Bhat <spbhat672@gmail.com>
2020-09-12 23:37:17 +0530

    Gngry login page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/GngryLogo.png';
+import passwrd from './img/forgotpassword.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Headerr">
+        <img src={logo} className="The-logo" alt="logo"/>          
+        </div>        
+        <div className="AppBody">        
+          <div className="LoginForm"> 
+             <table>
+
+               <row>
+                 <div className="Title">Welcome Back!</div>
+               </row>
+
+                <row>
+                  <div className="TextBox">
+                     <lable className="Label">User name</lable>
+                     <input type="text" className="Input"></input>
+                  </div>                 
+                </row>
+
+                <row>
+                  <div className="TextBox">
+                     <lable className="Label">Password</lable>
+                     <input type="password" className="Input"></input>
+                  </div>   
+                </row>
+
+                <row>
+                  <div className="ForgotPassword">
+                    <img src={passwrd} className="forgotImage" alt="?"/> 
+                    <href className="forgotText">Forgot password?</href>                    
+                  </div>   
+                </row>
+
+                <row>
+                  <div className="Button">
+                    <input type="submit" className="SignIn" value="Sign In"></input>
+                  </div>
+                </row>
+
+              </table>
+          </div>          
+        </div>
     </div>
   );
 }
